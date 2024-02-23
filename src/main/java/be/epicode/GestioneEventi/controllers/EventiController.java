9150771c9bffb03054282bef5e517af5a7dde713
Giordano <zagattigiordano@hotmail.it>
2024-02-23 16:53:51 +0100

    commit

--- src/main/java/be/epicode/GestioneEventi/controllers/EventiController.java
+++ src/main/java/be/epicode/GestioneEventi/controllers/EventiController.java
@@ -1,7 +1,38 @@
 package be.epicode.GestioneEventi.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import be.epicode.GestioneEventi.entities.Evento;
+import be.epicode.GestioneEventi.payloads.EventoDTO;
+import be.epicode.GestioneEventi.services.EventiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/eventi")
 public class EventiController {
+
+    @Autowired
+    private EventiService eventiService;
+
+    @GetMapping("")
+    public List<Evento> getAllEventi(){
+        return this.eventiService.getEventi();
+    }
+
+    @PostMapping("")
+    @PreAuthorize("hasAuthority('ORGANIZZATORE_EVENTI')")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Evento saveEvento(@RequestBody EventoDTO body){
+        return this.eventiService.saveEvento(body);
+    }
+
+    @PutMapping("/{eventoId}/users")
+    @PreAuthorize("hasAuthority('ORGANIZZATORE_EVENTI')")
+    public Evento assegnaPrenotazioni(@PathVariable int eventoId, @RequestBody List<Integer> users){
+        return eventiService.setPrenotazioni(eventoId,users);
+    }
+
 }
--- src/main/java/be/epicode/GestioneEventi/services/EventiService.java
+++ src/main/java/be/epicode/GestioneEventi/services/EventiService.java
@@ -18,22 +18,37 @@ public class EventiService {
     @Autowired
     private UsersService usersService;
 
-    public Evento findById(int id){
-        return eventiDAO.findById(id).orElseThrow(()-> new NotFoundException(id));
+    public Evento findById(int id) {
+        return eventiDAO.findById(id).orElseThrow(() -> new NotFoundException(id));
     }
 
-    public Evento saveEvento(EventoDTO body){
-        Evento newEvento = new Evento(body.titolo(),body.descrizione(),body.data(),body.luogo(),body.postiDisponibili());
+    public Evento saveEvento(EventoDTO body) {
+        Evento newEvento = new Evento(body.titolo(), body.descrizione(), body.data(), body.luogo(), body.postiDisponibili());
         return eventiDAO.save(newEvento);
     }
 
-    public Evento getPrenotazioni(int eventoId, List<Integer> usersIds){
+    public List<Evento> getEventi() {
+        return eventiDAO.findAll();
+    }
+
+    public Evento setPrenotazioni(int eventoId, List<Integer> usersIds) {
         Evento evento = findById(eventoId);
+        long postiDisponibili = evento.getPostiDisponibili();
         List<User> users = usersService.getUsersById(usersIds);
+
+        if (users.size() > postiDisponibili) {
+            throw new IllegalStateException("Non ci sono abbastanza posti disponibili per tutti gli utenti richiesti");
+        }
+
+        if (postiDisponibili - users.size() < 0) {
+            throw new IllegalStateException("Non ci sono abbastanza posti disponibili per tutti gli utenti richiesti");
+        }
+
         users.forEach(user -> {
             user.setEvento(evento);
         });
         evento.getUtenti().addAll(users);
+        evento.setPostiDisponibili(postiDisponibili - users.size());
         return eventiDAO.save(evento);
     }
 
